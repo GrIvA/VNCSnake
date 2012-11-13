befefdfeb8e341a9520f17daec72666ed7770d72
GrIvA <griva99@gmail.com>
2012-11-13 17:02:56 +0200

    reduction to the standard PEP8

--- daemon.py
+++ daemon.py
@@ -1,16 +1,17 @@
 # -*- coding: utf8 -*-
- 
+
 import os
 import atexit
 import sys
 import time
 from signal import SIGTERM
- 
+
+
 class Daemon():
     pidfile = 'pidfile.pid'
-    stdin='/dev/null'
-    stdout='/dev/null'
- 
+    stdin = '/dev/null'
+    stdout = '/dev/null'
+
     def daemonize(self):
         pid = os.fork()
         if pid == 0:
@@ -23,13 +24,13 @@ class Daemon():
                 sys.exit(0)
         else:
             sys.exit(0)
- 
+
         atexit.register(self.delpid)
-         
+
         pid = str(os.getpid())
-        file(self.pidfile,'w+').write("%s\n" % pid)
+        file(self.pidfile, 'w+').write("%s\n" % pid)
         return pid
- 
+
     def descriptors(self):
         sys.stdout.flush()
         sys.stderr.flush()
@@ -37,46 +38,46 @@ class Daemon():
         so = file(self.stdout, 'a+')
         os.dup2(si.fileno(), sys.stdin.fileno())
         os.dup2(so.fileno(), sys.stdout.fileno())
-  
+
     def delpid(self):
         os.remove(self.pidfile)
-  
-    def start(self,interactive=False):
+
+    def start(self, interactive=False):
         if interactive:
             print "Starting program in interactive mode"
             self.run()
             return
-         
+
         try:
-            pf = file(self.pidfile,'r')
+            pf = file(self.pidfile, 'r')
             pid = int(pf.read().strip())
             pf.close()
         except IOError:
             pid = None
-  
+
         if pid:
             message = "Daemon already running? (pid=%s)\n"
             sys.stderr.write(message % pid)
             sys.exit(1)
- 
+
         pid = self.daemonize()
         print "Starting daemon."
         self.descriptors()
         self.run()
-  
+
     def stop(self):
         try:
-            pf = file(self.pidfile,'r')
+            pf = file(self.pidfile, 'r')
             pid = int(pf.read().strip())
             pf.close()
         except IOError:
             pid = None
-  
+
         if not pid:
             message = "Daemon not running? (check %s)\n"
             sys.stderr.write(message % self.pidfile)
             return
- 
+
         print "Stopping daemon.\nWaiting for PID: %s" % pid
         try:
             while 1:
@@ -90,11 +91,11 @@ class Daemon():
             else:
                 print str(err)
                 sys.exit(1)
-  
+
     def restart(self):
         self.stop()
         self.start()
-         
+
     def run(self):
         print >> sys.stderr, "ERROR: Daemon not started\nYou need to override run() method in your subclass!"
         sys.exit(1)
--- vs_repeater.py
+++ vs_repeater.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
-  
+
 import sys
 import time
 import daemon
-
 import socket
-import sys
 import string
 
-FILTER=''.join([(len(repr(chr(x)))==3) and chr(x) or '.' for x in range(256)])
+FILTER = ''.join([(len(repr(chr(x))) == 3) and chr(x) or '.' for x in range(256)])
+
 
 def dump(src, length=8):
     """
     Функция отображающая дамп данных
     """
-    N=0; result=''
+    N = 0
+    result = ''
     while src:
-       s,src = src[:length],src[length:]
-       hexa = ' '.join(["%02X"%ord(x) for x in s])
-       s = s.translate(FILTER)
-       result += "%04X   %-*s   %s\n" % (N, length*3, hexa, s)
-       N+=length
+        s, src = src[:length], src[length:]
+        hexa = ' '.join(["%02X" % ord(x) for x in s])
+        s = s.translate(FILTER)
+        result += "%04X   %-*s   %s\n" % (N, length * 3, hexa, s)
+        N += length
     return result
 
 host = ''    # ip
@@ -29,15 +29,16 @@ port = 5000  # порт
 backlog = 5  # ожидаемое количество ожидающих обработки запросов
 size = 512   # размер данных
 
+
 class MyDaemon(daemon.Daemon):
     def __init__(self):
         daemon.Daemon.pidfile = "VNCSnake.pid"
-  
+
     def start(self, interactive=False):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       # создаём сокет для IPv4
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # устанавливаем опцию повторного использования порта для того, чтобы не ждать после останова сервера пока освободится порт
         try:
-            self.s.bind((host,port))  # ассоциировать адрес с сокетом
+            self.s.bind((host, port))  # ассоциировать адрес с сокетом
             self.s.listen(backlog)    # принимать запросы на установление соединения
         except Exception as e:
             print (e)
@@ -48,23 +49,24 @@ class MyDaemon(daemon.Daemon):
 
     def run(self):
         while True:
-            client, address = self.s.accept() # принять запрос и преобразовать в соединение. client - новое соединение
+            client, address = self.s.accept()   # принять запрос и преобразовать в соединение. client - новое соединение
             # print "server: got connection from %s port %d\n" % (address[0], address[1])
-            client.send("Welcome to server\n") # посылаем приглашение клиенту
-            data = client.recv(size) # получаем данные от клиента с размером size=1024
+            client.send("Welcome to server\n")  # посылаем приглашение клиенту
+            data = client.recv(size)            # получаем данные от клиента с размером size=1024
             while(len(data) > 0):
                 stream = string.split(data, '#')
-                if stream[0] == "quit": break; # если клиент вводит quit, то соединение с клиентом закрывается
+                if stream[0] == "quit":
+                    break                       # если клиент вводит quit, то соединение с клиентом закрывается
                 client.send("RECV: %d bytes\n" % len(data))
                 if stream[0] == "name":
                     client.send("NAME: %s\n" % stream[1])
                 client.send(dump(data))
                 data = client.recv(size)
-            client.close() # Закрыть соединение с клиентом
+            client.close()                      # Закрыть соединение с клиентом
             time.sleep(0.1)
-  
+
 my_daemon = MyDaemon()
-  
+
 if len(sys.argv) >= 2:
     if 'start' == sys.argv[1]:
         my_daemon.start()
