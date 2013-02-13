c42f3bd887d9a08c34a35f97b115fd7b172fb274
GrIvA <griva99@gmail.com>
2013-02-13 16:34:12 +0200

    begin server functionality.

--- vs_repeater.py
+++ vs_repeater.py
@@ -6,8 +6,10 @@ import time
 import daemon
 import socket
 import string
+import sqlite3
 
 FILTER = ''.join([(len(repr(chr(x))) == 3) and chr(x) or '.' for x in range(256)])
+myBase = 'VNCSnake.db'
 
 
 def dump(src, length=8):
@@ -20,7 +22,7 @@ def dump(src, length=8):
         s, src = src[:length], src[length:]
         hexa = ' '.join(["%02X" % ord(x) for x in s])
         s = s.translate(FILTER)
-        result += "%04X   %-*s   %s\n" % (N, length * 3, hexa, s)
+        result += "%04X   %-*s   %s\r\n" % (N, length * 3, hexa, s)
         N += length
     return result
 
@@ -32,6 +34,9 @@ size = 512   # размер данных
 
 class MyDaemon(daemon.Daemon):
     def __init__(self):
+        conn = sqlite3.connect(myBase)
+        conn.execute('DELETE FROM snake')
+        conn.close()
         daemon.Daemon.pidfile = "VNCSnake.pid"
 
     def start(self, interactive=False):
@@ -48,28 +53,48 @@ class MyDaemon(daemon.Daemon):
         daemon.Daemon.stop(self)
 
     def run(self):
+        conn = sqlite3.connect(myBase)
+        cur = conn.cursor()
+
         while True:
             client, address = self.s.accept()   # принять запрос и преобразовать в соединение. client - новое соединение
-            # print "server: got connection from %s port %d\n" % (address[0], address[1])
-            client.send("Welcome to server\n")  # посылаем приглашение клиенту
+            client.send("Welcome to server\r\n")  # посылаем приглашение клиенту
+            client.send("Connect from ip=%s, port=%d\r\n" % address)
             data = client.recv(size)            # получаем данные от клиента с размером size=1024
+
             while(len(data) > 0):
                 stream = string.split(data, '#')
-                if stream[0] == "quit":
-                    break                       # если клиент вводит quit, то соединение с клиентом закрывается
-                client.send("RECV: %d bytes\n" % len(data))
-                if stream[0] == "name":
-                    client.send("NAME: %s\n" % stream[1])
+                # if stream[0] == "quit":
+                    # break                       # если quit, то соединение с клиентом закрывается
+                client.send("RECV: %d bytes\r\n" % len(data))
+
+                if stream[0] == "share":
+                    client.send("SHARE: %s\r\n" % stream[1])
+
+                if stream[0] == "admin":
+                    cur.execute('select * from snake')
+                    rows = cur.fetchall()
+                    for row in rows:
+                        client.send("ADMIT: %s : %s : %s ==>\r\n" % (row[1], row[2], row[4]))
+
+                if stream[0] == "view":
+                    cur.execute('select * from snake')
+                    client.send("VIEW: %s\r\n" % stream[1])
+
+                if stream[0] == "close":
+                    client.send("CLOSE: %s\r\n" % stream[1])
+
                 client.send(dump(data))
                 data = client.recv(size)
             client.close()                      # Закрыть соединение с клиентом
             time.sleep(0.1)
+        conn.close()
 
 my_daemon = MyDaemon()
 
 if len(sys.argv) >= 2:
     if 'start' == sys.argv[1]:
-        my_daemon.start()
+        my_daemon.start(interactive=True)
     elif 'stop' == sys.argv[1]:
         my_daemon.stop()
     elif 'restart' == sys.argv[1]:
